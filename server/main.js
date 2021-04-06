4a3664f8f844df1b92ff3dae6b7011b5a2d9822a
Austin Harms <austin@harmsonline.com>
2021-04-06 15:02:29 -0500

    Created score DB with template

--- server/main.js
+++ server/main.js
@@ -1,18 +1,37 @@
 const sqlite = require('sqlite3');
+const fs = require("fs");
+const fsPromises = fs.promises;
 
-const openDB = async filePath => new Promise((resolve, reject) => { 
+const openDB = async (filePath, recreateFunc) => new Promise(async (resolve, reject) => { 
+  const fileExists = await fsPromises.access(filePath, fs.constants.W_OK).then(() => true).catch(() => false);
   const db = new sqlite.Database(filePath, err => {
     if(err)
       reject(err.message);
   });
+
+  if(!fileExists && recreateFunc !== undefined) {
+    recreateFunc(db);
+    console.log("DB recreated");
+  }
+
   resolve(db);
 });
 
+const recreateScoreDB = db => {
+  db.run(`CREATE TABLE IF NOT EXISTS playerScores (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nick TEXT NOT NULL,
+    time INTEGER NOT NULL,
+    weather TEXT NOT NULL,
+    collected TEXT NOT NULL,
+    level INTEGER NOT NULL
+  );`);
+};
+
 (async () => {
   try {
-    const DB = await openDB("./scores.db");
-    console.log(DB);
-    DB.close();
+    const db = await openDB("./scores.db", recreateScoreDB);
+    db.close();
   } catch(e) {
     console.log("Error: " + e);
   }
