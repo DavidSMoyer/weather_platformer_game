fe66dd7f1c0086bf69ee70d96863798c24890e94
Austin Harms <austin@harmsonline.com>
2021-04-12 13:08:16 -0500

    made level wait for nick name

--- www/js/popup.js
+++ www/js/popup.js
@@ -1,25 +1,37 @@
 // Handles popup events
-const errorPopup = document.querySelector("#error");
 let errorTimeout;
-const popup = document.querySelector("#popup");
-setTimeout(() => popup.classList.remove("hidden"), 100);
-const nickForm = document.querySelector("#nickname");
-nickForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const input = document.querySelector(".nick-input")
-  const nickPattern = /^[a-z0-9 ]+$/i;
-  if (nickPattern.test(input.value)) {
-    GlobalObject.userNick = input.value;
-    popup.classList.add("hidden");
-  } else {
-    displayError();
-  }
-});
 
 function displayError() {
+  const errorPopup = document.querySelector("#error");
   clearTimeout(errorTimeout);
   errorPopup.classList.remove("hidden");
   const input = document.querySelector(".nick-input");
   input.classList.add("invalid");
   errorTimeout = setTimeout(() => errorPopup.classList.add("hidden"), 2500);
+}
+
+async function showNickPopup() {
+  return new Promise((resolve, reject) => {
+    const popup = document.querySelector("#popup");
+    const nickForm = document.querySelector("#nickname");
+    popup.classList.remove("hidden");
+
+    nickForm.onsubmit = e => {
+      e.preventDefault();
+      const input = document.querySelector(".nick-input")
+      const nickPattern = /^[a-z0-9 ]+$/i;
+      if (nickPattern.test(input.value)) {
+        popup.classList.add("hidden");
+        resolve(input.value);
+      } else {
+        displayError();
+      }
+    };
+  });
+}
+
+async function showLevelPopup(level, time, coins, coinTotal, weather) {
+  return new Promise((resolve, reject) => {
+    resolve();
+  });
 }
